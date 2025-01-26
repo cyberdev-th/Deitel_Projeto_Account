ecb848ad39a16ace11aa0420e6111d17a8b464a7
Thiago Fernandes <devcyber.thiago@gmail.com>
2025-01-26 15:02:47 -0300

    Criação do construtor para a classe Account recebendo parâmetro name

--- Account.java
+++ Account.java
@@ -3,6 +3,10 @@ package ProjetoConta;
 public class Account {
     private String name;
 
+    public Account(String name){
+        this.name = name;
+    }
+
     public void setName(String name){
         this.name = name;
     }
--- AccountTest.java
+++ AccountTest.java
@@ -6,16 +6,11 @@ public class AccountTest {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Account myAccount = new Account();
+        Account account1 = new Account("Thiago Fernandes");
+        Account account2 = new Account("Nilo Nonnato");
 
-        System.out.printf("Nome inicial da conta: %s%n%n", myAccount.getName());
-
-        System.out.print("Por favor, informe o nome: ");
-        String accountName = input.nextLine();
-        myAccount.setName(accountName);
-        System.out.println();
-
-        System.out.printf("Nome atualizado do cliente: %s%n", myAccount.getName());
+        System.out.printf("Nome do cliente da conta 1: %s%n", account1.getName());
+        System.out.printf("Nome do cliente da conta 2: %s%n", account2.getName());
 
         input.close();
     }
